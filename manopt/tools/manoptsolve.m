7bde666b9353956be634751604cf9f0e788e596e
Victor <victorliao18@gmail.com>
2022-08-23 17:15:54 +0200

    temp

--- manopt/tools/manoptsolve.m
+++ manopt/tools/manoptsolve.m
@@ -1,4 +1,4 @@
-function [x, cost, info, options, time] = manoptsolve(problem, x0, options)
+function [x, cost, info, options] = manoptsolve(problem, x0, options)
 % Gateway helper function to call a Manopt solver, chosen in the options.
 %
 % function [x, cost, info, options] = manoptsolve(problem)
@@ -47,11 +47,10 @@ function [x, cost, info, options, time] = manoptsolve(problem, x0, options)
     if ~exist('x0', 'var')
         x0 = [];
     end
-    f = @() options.solver(problem, x0, options);
+
     % Issue the actual call.
     [x, cost, info, options] = options.solver(problem, x0, options);
-%     time = timeit(f);
-    time = 1;
+    [x, cost, info, options] = options.solver(problem, x0, options);
     if ~isfield(info, 'memorytCG_MB')
         for i=1:length(info)
             info(i).memorytCG_MB = 0;
